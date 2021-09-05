819346f009d0fa7e047ea15b23ca934072ed992c
iamrahultanwar <mr.rahultanwar@gmail.com>
2021-09-05 15:18:49 +0530

    🚀 RELEASE: v0.0.15 : ts support added

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const figlet = require("figlet");
 const command = require("./lib/commands");
 clear();
 
+console.log();
 // Print Hydyco
 console.log(
   chalk.greenBright(figlet.textSync("Hydyco", { horizontalLayout: "full" }))
 );
+
+console.log();
 
 const run = async () => {
   command.getCommand();
